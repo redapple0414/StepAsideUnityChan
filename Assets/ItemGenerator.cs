19155a3d89067b6f50126e1bf3201c7e9542b37c
redapple0414 <77644441+redapple0414@users.noreply.github.com>
2021-02-07 18:42:06 +0900

    Initial commit

---
+++ Assets/ItemGenerator.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+
+using UnityEngine;
+
+public class ItemGenerator : MonoBehaviour
+{
+    public GameObject carPrefab;
+    public GameObject coinPrefab;
+    public GameObject conePrefab;
+    private int startPos = 80;
+    private float goalPos = 360;
+    private float posRange = 3.4f;
+    private GameObject unitychan;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+  
+        for (int i = startPos; i < goalPos; i += 15)
+        {
+            int num = Random.Range(1, 11);
+            if (num <= 2)
+            {
+
+                for (float j = -1; j <= 1; j += 0.4f)
+                {
+                    GameObject cone = Instantiate(conePrefab);
+                    cone.transform.position = new Vector3(4 * j, cone.transform.position.y, i);
+                }
+            }
+
+
+            else
+            {
+
+
+                for (int j = -1; j <= 1; j++)
+                {
+
+                    int item = Random.Range(1, 11);
+
+                    int offsetZ = Random.Range(-5, 6);
+
+                    if (1 <= item && item <= 6)
+                    {
+
+                        GameObject coin = Instantiate(coinPrefab);
+                        coin.transform.position = new Vector3(posRange * j, coin.transform.position.y, i + offsetZ);
+                    }
+                    else if (7 <= item && item <= 9)
+                    {
+
+                        GameObject car = Instantiate(carPrefab);
+                        car.transform.position = new Vector3(posRange * j, car.transform.position.y, i + offsetZ);
+                    }
+                }
+            }
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.unitychan = GameObject.Find("unitychan");
+
+        Debug.Log(this.unitychan.transform.position.z);
+        if (unitychan.transform.position.z > this.transform.position.z)
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+
+
+}
